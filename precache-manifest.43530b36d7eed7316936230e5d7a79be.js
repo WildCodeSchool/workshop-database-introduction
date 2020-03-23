bef2d73f7ab982e2e73d52f0ddbc60676c68410e
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2020-03-23 12:07:13 +0100

    Updates

---
+++ precache-manifest.43530b36d7eed7316936230e5d7a79be.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852cd388e486b96eda2ac2840e1dd1ce",
+    "url": "/workshop-database-introduction/index.html"
+  },
+  {
+    "revision": "7a93aaadd1c7ab8bafd6",
+    "url": "/workshop-database-introduction/static/js/2.42ef4b40.chunk.js"
+  },
+  {
+    "revision": "0de1532dbf7b2883f22a",
+    "url": "/workshop-database-introduction/static/js/main.f985c40a.chunk.js"
+  },
+  {
+    "revision": "9527b90ceac96bf25a6e",
+    "url": "/workshop-database-introduction/static/js/runtime~main.68f9ff25.js"
+  }
+]);
